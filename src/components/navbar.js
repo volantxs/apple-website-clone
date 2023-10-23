ce69d1d302d7d0670cf892b601d4c7178062301c
volantxs <volantxs@gmail.com>
2023-10-23 23:02:05 +0530

    added react-icons on the navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { BsSearch } from 'react-icons/bs';
+import  {CgShoppingBag } from 'react-icons/cg';
+import  {BsApple } from 'react-icons/bs';
 
 const Navbar = () => {
     return (
-        <div className='navbar'>
+        <div className='navbar-nav'>
 
+        <div className='navbar'>
             <span>
-                <Link to="/" style={{color: "white", textDecoration: "none"}}>Apple</Link>
+                <Link to="/" style={{color: "white", textDecoration: "none"}}><BsApple size={16} /></Link>
             </span>
             <span>
                 <Link to="/" style={{color: "white", textDecoration: "none"}}>Store</Link>
@@ -39,13 +43,14 @@ const Navbar = () => {
                 <Link to="/" style={{color: "white", textDecoration: "none"}}>Support</Link>
             </span>
             <span>
-                <Link to="/" style={{color: "white", textDecoration: "none"}}>Search</Link>
+                <Link to="/" style={{color: "white", textDecoration: "none"}}><BsSearch size={14} style={{position: "relative",top:"2px"}} /></Link>
             </span>
             <span>
-                <Link to="/" style={{color: "white", textDecoration: "none"}}>bag</Link>
+                <Link to="/" style={{color: "white", textDecoration: "none"}}><CgShoppingBag size={16}  /></Link>
             </span>
             </div>
-
+        </div>
+            
     )
 }
 
