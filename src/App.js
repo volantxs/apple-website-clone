85631c89cb300e3d4f8c85380a3b903a83039f82
volantxs <volantxs@gmail.com>
2023-10-24 02:08:02 +0530

    added hero section heading

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Navbar from './components/navbar';
+import Hero from './components/hero';
 
 function App() {
   return (
   <Router>
   <Navbar/>
+  <div>
+  <Hero/>
+  </div>
   </Router>
   );
 }
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,4 +1,3 @@
-import React from 'react';
 import { Link } from 'react-router-dom';
 import { BsSearch } from 'react-icons/bs';
 import  {CgShoppingBag } from 'react-icons/cg';
@@ -6,50 +5,48 @@ import  {BsApple } from 'react-icons/bs';
 
 const Navbar = () => {
     return (
-        <div className='navbar-nav'>
 
-        <div className='navbar'>
+        <nav className='navbar'>
             <span>
-                <Link to="/" style={{color: "white", textDecoration: "none"}}><BsApple size={16} /></Link>
+                <Link to="/" style={{textDecoration: "none"}}><BsApple size={16} /></Link>
             </span>
             <span>
-                <Link to="/" style={{color: "white", textDecoration: "none"}}>Store</Link>
+                <Link to="/" style={{ textDecoration: "none"}}>Store</Link>
             </span>
             <span>
-                <Link to="/" style={{color: "white", textDecoration: "none"}}>Mac</Link>
+                <Link to="/" style={{ textDecoration: "none"}}>Mac</Link>
             </span>
             <span>
-                <Link to="/" style={{color: "white", textDecoration: "none"}}>iPad</Link>
+                <Link to="/" style={{ textDecoration: "none"}}>iPad</Link>
             </span>
             <span>
-                <Link to="/" style={{color: "white", textDecoration: "none"}}>iPhone</Link>
+                <Link to="/" style={{ textDecoration: "none"}}>iPhone</Link>
             </span>
             <span>
-                <Link to="/" style={{color: "white", textDecoration: "none"}}>Watch</Link>
+                <Link to="/" style={{ textDecoration: "none"}}>Watch</Link>
             </span>
             <span>
-                <Link to="/" style={{color: "white", textDecoration: "none"}}>AirPods</Link>
+                <Link to="/" style={{ textDecoration: "none"}}>AirPods</Link>
             </span>
             <span>
-                <Link to="/" style={{color: "white", textDecoration: "none"}}>TV & Home</Link>
+                <Link to="/" style={{ textDecoration: "none"}}>TV & Home</Link>
             </span>
             <span>
-                <Link to="/" style={{color: "white", textDecoration: "none"}}>Entertainment</Link>
+                <Link to="/" style={{ textDecoration: "none"}}>Entertainment</Link>
             </span>
             <span>
-                <Link to="/" style={{color: "white", textDecoration: "none"}}>Accessories</Link>
+                <Link to="/" style={{ textDecoration: "none"}}>Accessories</Link>
             </span>
             <span>
-                <Link to="/" style={{color: "white", textDecoration: "none"}}>Support</Link>
+                <Link to="/" style={{ textDecoration: "none"}}>Support</Link>
             </span>
             <span>
-                <Link to="/" style={{color: "white", textDecoration: "none"}}><BsSearch size={14} style={{position: "relative",top:"2px"}} /></Link>
+                <Link to="/" style={{ textDecoration: "none"}}><BsSearch size={14} style={{position: "relative",top:"2px"}} /></Link>
             </span>
             <span>
-                <Link to="/" style={{color: "white", textDecoration: "none"}}><CgShoppingBag size={16}  /></Link>
+                <Link to="/" style={{ textDecoration: "none"}}><CgShoppingBag size={16}  /></Link>
             </span>
-            </div>
-        </div>
+            </nav>
             
     )
 }
