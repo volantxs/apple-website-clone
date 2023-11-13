6253d97ed01bfbd48a484f9c7a278de390d5e72d
volantxs <volantxs@gmail.com>
2023-11-13 21:51:13 +0530

    fixed accessories section to be responsive

--- src/components/accessories.js
+++ src/components/accessories.js
@@ -8,16 +8,84 @@ const Accessories = () => {
         <header>
            <span style={{color: "black"}}>Accesories.</span> Festive gift ideas.
         </header>
-        <div   className="latest_slider"  >
+        <div   className="latest_slider desktop"  >
         <div className="acce_head a_one">
         <h5>FABULOUSLY FESTIVE</h5>
             <h1>Curated accessories to spread joy.</h1>
             </div>
             <div className="cara">
-
         <Carousel 
           responsive={responsive} 
           >
+            <div className="acce_item a_two">
+            <h5>NEW</h5>
+            <h3>iPhone 15 Pro FineWoven Case with MagSafe - Taupe</h3>
+            <p>MRP ₹5900.00 (incl. of all taxes)</p>
+            <div className="dot">
+              <span style={{backgroundColor: "#B0578D"}}></span>
+              <span style={{backgroundColor: "#618264"}}></span>
+              <span style={{backgroundColor: "#C8AE7D"}}></span>
+              <span style={{backgroundColor: "#749BC2"}}></span>
+              <span style={{backgroundColor: "#7D7C7C"}}></span>
+            </div>
+            </div>
+            <div className="acce_item a_three">
+            <h5>NEW</h5>
+            <h3>iPhone FineWoven Wallet with MagSafe - Mulberry</h3>
+            <p>MRP ₹5900.00 (incl. of all taxes)</p>
+            <div className="dot">
+              <span style={{backgroundColor: "#B0578D"}}></span>
+              <span style={{backgroundColor: "#618264"}}></span>
+              <span style={{backgroundColor: "#C8AE7D"}}></span>
+              <span style={{backgroundColor: "#749BC2"}}></span>
+              <span style={{backgroundColor: "#7D7C7C"}}></span>
+            </div>
+            </div>
+            <div className="acce_item a_four">
+            <h5>FREE ENGRAVING</h5>
+            <h3>AirTag</h3>
+            <p>MRP ₹3490.00 (incl. of all taxes)</p>
+            
+            </div>
+            <div className="acce_item a_five">
+            <h5>NEW</h5>
+            <h3>Apple TV 4K Wi-Fi with 64GB storage</h3>
+            <p>MRP ₹14900.00 (incl. of all taxes)</p>
+            </div>
+            <div className="acce_item a_six">
+            <h5>NEW</h5>
+            <h3>iPhone 15 FineWoven Case with MagSafe - Pacific Blue</h3>
+            <p>MRP ₹5900.00 (incl. of all taxes)</p>
+            <div className="dot">
+              <span style={{backgroundColor: "#B0578D"}}></span>
+              <span style={{backgroundColor: "#618264"}}></span>
+              <span style={{backgroundColor: "#C8AE7D"}}></span>
+              <span style={{backgroundColor: "#749BC2"}}></span>
+              <span style={{backgroundColor: "#7D7C7C"}}></span>
+            </div>
+            </div>
+            <div className="acce_item a_seven" >
+            <h5>NEW</h5>
+            <h3>41mm Mulberry Modern Buckle - Medium</h3>
+            <p>MRP ₹14500.00 (incl. of all taxes)</p>
+            <div className="dot_w">
+              <span style={{backgroundColor: "#9DB2BF"}}></span>
+              <span style={{backgroundColor: "#662549"}}></span>
+              <span style={{backgroundColor: "#B0926A"}}></span>
+            </div>
+            </div>
+        </Carousel>
+        </div>
+        </div>
+        <div   className="latest_slider mobile"  >
+            <div className="cara">
+        <Carousel 
+          responsive={responsive} 
+          >
+              <div className="acce_head a_one">
+        <h5>FABULOUSLY FESTIVE</h5>
+            <h1>Curated accessories to spread joy.</h1>
+            </div>
             <div className="acce_item a_two">
             <h5>NEW</h5>
             <h3>iPhone 15 Pro FineWoven Case with MagSafe - Taupe</h3>
@@ -96,7 +164,7 @@ const responsive = {
     },
     mobile: {
       breakpoint: { max: 464, min: 0 },
-      items: 1
+      items: 1.18
     }
   };
 
--- src/components/audio.js
+++ src/components/audio.js
@@ -8,7 +8,7 @@ const Audio = () => {
         <header>
            <span style={{color: "black"}}>Loud and clear.</span> Audio that's in harmony with the festivies.
         </header>
-        <div   className="audio_slider"  >
+        <div   className="audio_slider desktop"  >
         <div className="audio_head">
         <h5>APPLE MUSIC OFFER</h5>
             <h1>Get Apple Music Free for 6 months with Homepod and Airpods.</h1>
@@ -17,6 +17,64 @@ const Audio = () => {
         <Carousel 
           responsive={responsive} 
           >
+            <div className="audio_item u_two">
+            <h5>FREE ENGRAVING</h5>
+            <h3>AirPods Pro (2nd generation) with MagSafe Charging Case...</h3>
+            <p>MRP ₹24900.00 (incl. of all taxes)</p>
+            </div>
+            <div className="audio_item u_three exPad">
+            <h3>HomePod - Midnight</h3>
+            <p>MRP ₹32900.00 (incl. of all taxes)</p>
+            <div className="dot_au">
+              <span style={{backgroundColor: "#45474B"}}></span>
+              <span style={{backgroundColor: "#F5F5F5"}}></span>
+            </div>
+            </div>
+            <div className="audio_item u_four exPad">
+            <h3>HomePod min - Orange</h3>
+            <p>MRP ₹10900.00 (incl. of all taxes)</p>
+            <div className="dot_n">
+              <span style={{backgroundColor: "#45474B"}}></span>
+              <span style={{backgroundColor: "#2E4374"}}></span>
+              <span style={{backgroundColor: "#FFCC70"}}></span>
+              <span style={{backgroundColor: "#F5F5F5"}}></span>
+              <span style={{backgroundColor: "#ED7D31"}}></span>
+            </div>
+            </div>
+            <div className="audio_item u_five exPad">
+            <h3>AirPods Max - Sky Blue</h3>
+            <p>MRP ₹59900.00 (incl. of all taxes)</p>
+            <div className="dot_hd">
+                <img src="https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/airpods-max-select-spacegray-202011_SW_COLOR?wid=64&hei=64&fmt=jpeg&qlt=90&.v=1604622935000" />
+                <img src="https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/airpods-max-select-pink-202011_SW_COLOR?wid=64&hei=64&fmt=jpeg&qlt=90&.v=1603846873000" />
+                <img src="https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/airpods-max-select-green-202011_SW_COLOR?wid=64&hei=64&fmt=jpeg&qlt=90&.v=1603846873000" />
+                <img src="https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/airpods-max-select-silver-202011_SW_COLOR?wid=64&hei=64&fmt=jpeg&qlt=90&.v=1603846873000" />
+                <img src="https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/airpods-max-select-skyblue-202011_SW_COLOR?wid=64&hei=64&fmt=jpeg&qlt=90&.v=1603846875000" />
+            </div>
+            </div>
+            <div className="audio_item u_six">
+            <h5>FREE ENGRAVING</h5>
+            <h3>AirPods (3rd generation) with Lightning Charging Case</h3>
+            <p>MRP ₹19900.00 (incl. of all taxes)</p>
+
+            </div>
+            <div className="audio_item u_seven " >
+            <h5>FREE ENGRAVING</h5>
+            <h3>AirPods (2nd generation)</h3>
+            <p>MRP ₹12900.00 (incl. of all taxes)</p>
+            </div>
+        </Carousel>
+        </div>
+        </div>
+        <div   className="audio_slider mobile"  >
+            <div className="cara">
+        <Carousel 
+          responsive={responsive} 
+          >
+             <div className="audio_head">
+        <h5>APPLE MUSIC OFFER</h5>
+            <h1>Get Apple Music Free for 6 months with Homepod and Airpods.</h1>
+            </div>
             <div className="audio_item u_two">
             <h5>FREE ENGRAVING</h5>
             <h3>AirPods Pro (2nd generation) with MagSafe Charging Case...</h3>
@@ -84,7 +142,7 @@ const responsive = {
     },
     mobile: {
       breakpoint: { max: 464, min: 0 },
-      items: 1
+      items: 1.18
     }
   };
 
